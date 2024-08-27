586cd0b2f8dc0e66231f7513ec24892683e53a01
Leticia <lmossicardoso@gmail.com>
2024-08-27 20:16:18 -0300

    Base MVC

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php
+        require './Application/autoload.php';
+        use Application\core\App;
+        use Application\core\Controller;
+        $app = new App();
+    ?>
+</head>
+<body>
+</body>
+</html>
