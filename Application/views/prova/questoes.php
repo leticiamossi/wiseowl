4adb95690cc934110241cb634953530863091d98
leticia <lmossicardoso@gmail.com>
2024-11-23 20:19:36 -0300

    correcao de fonte

--- Application/views/prova/questoes.php
+++ Application/views/prova/questoes.php
@@ -10,6 +10,7 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Andada+Pro:ital,wght@0,400..840;1,400..840&family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Purple+Purse&display=swap" rel="stylesheet">
 
         <link rel="stylesheet" href="../../../public/assets/css/Header/style.css">
         <link rel="stylesheet" href="../../../public/assets/css/Padrao/style.css">
