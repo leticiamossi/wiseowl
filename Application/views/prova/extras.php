a09caf60f1d6cae71b731120e511bfab48aede4a
leticia <lmossicardoso@gmail.com>
2024-11-23 01:08:23 -0300

    Corrigindo titulos

--- Application/views/prova/extras.php
+++ Application/views/prova/extras.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WiseOwl | Home</title>
+    <title>WiseOwl | Prova</title>
     <link rel="shortcut icon" href="../../../public/assets/img/Logo/Icon-verde.png" type="image/x-icon">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
